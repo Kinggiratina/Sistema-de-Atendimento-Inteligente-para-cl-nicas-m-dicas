05cfabb27bcb1b7f92bdfbed365a0da50aafa398
Kinggiratina <120383870+Kinggiratina@users.noreply.github.com>
2026-03-20 16:30:16 -0300

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const {
-  PORT = 4000,
-  MONGO_URI,
-  JWT_SECRET,
-  WEATHER_API_KEY,
-  WEATHER_DEFAULT_CITY = 'Sao Paulo,BR',
-  CLIENT_ORIGIN = 'http://localhost:5173'
+PORT=4000
+MONGO_URI=mongodb+srv://mongodb+srv://josecarlosjcsjc0_db_user:<db_password>@cluster0.3r8vu1i.mongodb.net/
+JWT_SECRET=uma_chave_bem_segura_aqui
+WEATHER_API_KEY=sua_chave_openweather
+WEATHER_DEFAULT_CITY=Sao Paulo,BR
+CLIENT_ORIGIN=http://localhost:5173
 } = process.env;
